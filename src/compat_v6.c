86148fb69b149aa08318620539e2e2350fa85544
Liran Alon <liran@amazon.com>
2023-08-14 18:30:25 +0300

    TODO: Document memory leaks due to NCCL ext-net plugin API issues
    Signed-off-by: Liran Alon <liran@amazon.com>

--- src/compat_v6.c
+++ src/compat_v6.c
@@ -6,6 +6,20 @@
 
 #include "nccl_ofi.h"
 
+/*
+ * TODO: The plugin is missing an API for cleanup.
+ *
+ * 1. This results in having various memory leaks because nccl_net_ofi_init()
+ * allocates resources that are never freed. Such as never calling device->scheduler->fini()
+ * to free the blocks allocated by scheduler's internal freelist.
+ *
+ * 2. The getProperties() API is expected to fill struct ncclNetProperties_v6_t. This struct
+ * defines `pciPath` member as pointer to string that is allocated by plugin. However, this
+ * memory is never freed as it is not well defined for NCCL that it can free it with free()
+ * and there is no plugin API to free previously allocated properties resources.
+ * Probably the easiest way to address this is to introduce a v7 version of the structure
+ * that changes `name` and `pciPath` fields to char arrays with limited length rather than pointers.
+ */
 const ncclNet_v6_t ncclNetPlugin_v6 = {
         .name = "AWS Libfabric",
         .init = nccl_net_ofi_init,
